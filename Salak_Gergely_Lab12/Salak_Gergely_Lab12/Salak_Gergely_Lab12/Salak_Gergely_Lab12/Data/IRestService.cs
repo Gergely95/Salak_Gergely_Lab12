a8d0cf0d23cca4549225ceb9ab61659a191689d6
Salak Gergely <92632380+Gergely95@users.noreply.github.com>
2022-01-15 19:19:54 +0200

    Add project files.

---
+++ Salak_Gergely_Lab12/Salak_Gergely_Lab12/Salak_Gergely_Lab12/Salak_Gergely_Lab12/Data/IRestService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Salak_Gergely_Lab12.Models;
+
+namespace Salak_Gergely_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
